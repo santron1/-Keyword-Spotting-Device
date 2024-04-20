3909d897b8670ac277fa98ae4a2336f50d0b518b
santron1 <167627757+santron1@users.noreply.github.com>
2024-04-20 21:57:41 +0530

    Add files via upload

---
+++ hey_ultron/hey_ultron.ino
@@ -0,0 +1,240 @@
+#define EIDSP_QUANTIZE_FILTERBANK   1
+
+#include <sanchay-project-1_inferencing.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/i2s.h"
+
+/** Define the GPIO pin connected to the ESP32's built-in LED */
+#define LED_GPIO_PIN    GPIO_NUM_2
+
+/** Audio buffers, pointers and selectors */
+typedef struct {
+    int16_t *buffer;
+    uint8_t buf_ready;
+    uint32_t buf_count;
+    uint32_t n_samples;
+} inference_t;
+
+static inference_t inference;
+static const uint32_t sample_buffer_size = 2048;
+static signed short sampleBuffer[sample_buffer_size];
+static bool debug_nn = false;
+static bool record_status = true;
+
+// Define LED control variables
+bool led_on = false; // Flag to indicate whether LED is on
+unsigned long led_start_time = 0; // Variable to store the start time of LED activation
+
+void setup() {
+    Serial.begin(115200);
+    while (!Serial);
+    Serial.println("Edge Impulse Inferencing Demo");
+
+    ei_printf("Inferencing settings:\n");
+    ei_printf("\tInterval: ");
+    ei_printf_float((float)EI_CLASSIFIER_INTERVAL_MS);
+    ei_printf(" ms.\n");
+    ei_printf("\tFrame size: %d\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+    ei_printf("\tSample length: %d ms.\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT / 16);
+    ei_printf("\tNo. of classes: %d\n", sizeof(ei_classifier_inferencing_categories) / sizeof(ei_classifier_inferencing_categories[0]));
+
+    ei_printf("\nStarting continuous inference in 2 seconds...\n");
+    ei_sleep(2000);
+
+    if (microphone_inference_start(EI_CLASSIFIER_RAW_SAMPLE_COUNT) == false) {
+        ei_printf("ERR: Could not allocate audio buffer (size %d), this could be due to the window length of your model\r\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT);
+        return;
+    }
+
+    // Initialize the GPIO pin connected to the LED as an output
+    gpio_pad_select_gpio(LED_GPIO_PIN);
+    gpio_set_direction(LED_GPIO_PIN, GPIO_MODE_OUTPUT);
+
+    ei_printf("Recording...\n");
+}
+
+void loop() {
+    bool m = microphone_inference_record();
+    if (!m) {
+        ei_printf("ERR: Failed to record audio...\n");
+        return;
+    }
+
+    signal_t signal;
+    signal.total_length = EI_CLASSIFIER_RAW_SAMPLE_COUNT;
+    signal.get_data = &microphone_audio_signal_get_data;
+    ei_impulse_result_t result = { 0 };
+
+    EI_IMPULSE_ERROR r = run_classifier(&signal, &result, debug_nn);
+    if (r != EI_IMPULSE_OK) {
+        ei_printf("ERR: Failed to run classifier (%d)\n", r);
+        return;
+    }
+
+    // Print the predictions
+    ei_printf("Predictions ");
+    ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
+        result.timing.dsp, result.timing.classification, result.timing.anomaly);
+    ei_printf(": \n");
+    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+        ei_printf("    %s: ", result.classification[ix].label);
+        ei_printf_float(result.classification[ix].value);
+        ei_printf("\n");
+    }
+
+    // Check if "hey_ultron" is detected and its value is >= 0.9
+    if (strcmp(result.classification[0].label, "hey_ultron") == 0 && result.classification[0].value >= 0.9) {
+        // Turn on the built-in LED and start the timer
+        led_on = true;
+        led_start_time = millis();
+        gpio_set_level(LED_GPIO_PIN, 1);
+    }
+
+    // Check if the LED has been on for 3 seconds
+    if (led_on && millis() - led_start_time >= 3000) {
+        // Turn off the built-in LED and reset the timer
+        led_on = false;
+        gpio_set_level(LED_GPIO_PIN, 0);
+    }
+}
+
+static void audio_inference_callback(uint32_t n_bytes) {
+    for(int i = 0; i < n_bytes>>1; i++) {
+        inference.buffer[inference.buf_count++] = sampleBuffer[i];
+
+        if(inference.buf_count >= inference.n_samples) {
+          inference.buf_count = 0;
+          inference.buf_ready = 1;
+        }
+    }
+}
+
+static void capture_samples(void* arg) {
+    const int32_t i2s_bytes_to_read = (uint32_t)arg;
+    size_t bytes_read = i2s_bytes_to_read;
+
+    while (record_status) {
+        /* read data at once from i2s */
+        i2s_read((i2s_port_t)1, (void*)sampleBuffer, i2s_bytes_to_read, &bytes_read, 100);
+
+        if (bytes_read <= 0) {
+            ei_printf("Error in I2S read : %d", bytes_read);
+        }
+        else {
+            if (bytes_read < i2s_bytes_to_read) {
+                ei_printf("Partial I2S read");
+            }
+
+            // Scale the data (otherwise the sound is too quiet)
+            for (int x = 0; x < i2s_bytes_to_read/2; x++) {
+                sampleBuffer[x] = (int16_t)(sampleBuffer[x]) * 8;
+            }
+
+            if (record_status) {
+                audio_inference_callback(i2s_bytes_to_read);
+            }
+            else {
+                break;
+            }
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+static bool microphone_inference_start(uint32_t n_samples) {
+    inference.buffer = (int16_t *)malloc(n_samples * sizeof(int16_t));
+
+    if(inference.buffer == NULL) {
+        return false;
+    }
+
+    inference.buf_count  = 0;
+    inference.n_samples  = n_samples;
+    inference.buf_ready  = 0;
+
+    if (i2s_init(EI_CLASSIFIER_FREQUENCY)) {
+        ei_printf("Failed to start I2S!");
+    }
+
+    ei_sleep(100);
+
+    record_status = true;
+
+    xTaskCreate(capture_samples, "CaptureSamples", 1024 * 32, (void*)sample_buffer_size, 10, NULL);
+
+    return true;
+}
+
+static bool microphone_inference_record(void) {
+    bool ret = true;
+
+    while (inference.buf_ready == 0) {
+        delay(10);
+    }
+
+    inference.buf_ready = 0;
+    return ret;
+}
+
+static int microphone_audio_signal_get_data(size_t offset, size_t length, float *out_ptr) {
+    numpy::int16_to_float(&inference.buffer[offset], out_ptr, length);
+
+    return 0;
+}
+
+static void microphone_inference_end(void) {
+    i2s_deinit();
+    ei_free(inference.buffer);
+}
+
+static int i2s_init(uint32_t sampling_rate) {
+  // Start listening for audio: MONO @ 8/16KHz
+  i2s_config_t i2s_config = {
+      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_TX),
+      .sample_rate = sampling_rate,
+      .bits_per_sample = (i2s_bits_per_sample_t)16,
+      .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
+      .communication_format = I2S_COMM_FORMAT_I2S,
+      .intr_alloc_flags = 0,
+      .dma_buf_count = 8,
+      .dma_buf_len = 512,
+      .use_apll = false,
+      .tx_desc_auto_clear = true,
+      .fixed_mclk = -1,
+  };
+  i2s_pin_config_t pin_config = {
+      .bck_io_num = 26,    // IIS_SCLK
+      .ws_io_num = 32,     // IIS_LCLK
+      .data_out_num = -1,  // IIS_DSIN
+      .data_in_num = 33,   // IIS_DOUT
+  };
+  esp_err_t ret = 0;
+
+  ret = i2s_driver_install((i2s_port_t)1, &i2s_config, 0, NULL);
+  if (ret != ESP_OK) {
+    ei_printf("Error in i2s_driver_install");
+  }
+
+  ret = i2s_set_pin((i2s_port_t)1, &pin_config);
+  if (ret != ESP_OK) {
+    ei_printf("Error in i2s_set_pin");
+  }
+
+  ret = i2s_zero_dma_buffer((i2s_port_t)1);
+  if (ret != ESP_OK) {
+    ei_printf("Error in initializing dma buffer with 0");
+  }
+
+  return int(ret);
+}
+
+static int i2s_deinit(void) {
+    i2s_driver_uninstall((i2s_port_t)1); //stop & destroy i2s driver
+    return 0;
+}
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_MICROPHONE
+#error "Invalid model for current sensor."
+#endif
